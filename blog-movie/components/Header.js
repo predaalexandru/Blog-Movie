1d4786b8bb1c6594488a695bb413e33acfbd1312
Preda Alexandru <alexpreda95@yahoo.com>
2020-12-29 21:06:33 +0200

    Adding some packages for css/js and create Header

--- blog-movie/components/Header.js
+++ blog-movie/components/Header.js
@@ -1,7 +1,32 @@
+import styled from '@emotion/styled'
+import {rem} from 'polished'
+
 export default function Header() {
     return (
-        <header>
-            The real header!
-        </header>
+        <HeaderStyled>
+            <div className="container">
+                <div className="logo">
+                    <img src="#" alt="Sites Logo" />
+                    <span className="logo-text">Next Movies</span>
+                </div>
+            </div>
+        </HeaderStyled>
     )
 }
+
+const HeaderStyled = styled.header`
+    background: #efefef;
+    padding: 20px;
+
+    .logo {
+        display: flex;
+        align-items: center;
+
+        .logo-text {
+            color: #333333;
+            font-weight: bold;
+            font-size: ${rem(20)};
+            margin-left: 20px;
+        }
+    }
+` 
--- blog-movie/pages/_app.js
+++ blog-movie/pages/_app.js
@@ -1,11 +1,22 @@
-import '../styles/globals.css'
 import Header from '../components/Header'
+import {ThemeProvider} from '@emotion/react'
+import GlobalStyles from '../components/GlobalStyles/GlobalStyles'
+
+
+const theme = {
+  colors: {
+    primary: '#ff0000'
+  }
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
